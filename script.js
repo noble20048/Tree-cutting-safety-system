96ae7b55221dc4f4314a71dd4c9274604e0a48d9
NoblebijuNBNL <noblebijukattungal@gmail.com>
2026-02-08 21:01:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function assessTree() {
-    const height = document.getElementById("treeHeight").value;
-    const circumference = document.getElementById("trunkCircumference").value
-    const wind = document.getElementById("windSpeed").value
+    const height = Number(document.getElementById("treeHeight").value);
+    const circumference = document.getElementById("trunkCircumference").value;
+    const wind = Number(document.getElementById("windSpeed").value);
 
     document.getElementById("result").innerText = 
     "Tree Height: " + height + " m, " +
